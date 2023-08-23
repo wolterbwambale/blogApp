c390609be431d22a605982a9be0f9da97e860239
wolterbwambale <benplaan1@gmail.com>
2023-08-23 09:58:50 +0300

    creating rails with postgres

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  has_many :posts, foreign_key: 'author_id'
+  has_many :comments
+  has_many :likes
+
+  validates_presence_of :name
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  after_create :recent_posts
+
+  def recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
+end
