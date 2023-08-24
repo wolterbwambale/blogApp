ac844a86e5aaf808cf58b9505d9cf3d9d75ffa90
wolterbwambale <benplaan1@gmail.com>
2023-08-24 03:33:50 +0300

    Add method that updates the likes counter for a post.

---
+++ app/models/like.rb
@@ -0,0 +1,11 @@
+class Like < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+
+  # Method to update the likes counter for a post
+  after_create :update_post_likes_counter
+
+  def update_post_likes_counter
+    post.increment(:likes_counter)
+  end
+end
