1cf0a135a6045e0209ab591ef2ece46a25e454c4
wolterbwambale <benplaan1@gmail.com>
2023-08-24 15:12:28 +0300

    modify the model

--- app/models/posts.rb
+++ app/models/posts.rb
@@ -1,19 +1,2 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_many :comments
-  has_many :likes
-
-  validates_presence_of :title, :text
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-
-  scope :recent, -> { order(created_at: :desc) }
-
-  def recent_comments(limit = 5)
-    comments.order(created_at: :desc).limit(limit)
-  end
-
-  def total_likes
-    likes_counter || likes.count
-  end
 end
