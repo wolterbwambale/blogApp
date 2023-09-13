028101e44cd51729507329dba9a33fa1cd461bf1
wolterbwambale <benplaan1@gmail.com>
2023-09-13 22:35:05 +0300

    update post contrller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   def index
     @user = User.find_by(id: params[:user_id])
     @posts = @user.posts if @user
-    @posts = Post.paginate(page: params[:page], per_page: 2)
+    @posts = @user.posts.includes(:comments).paginate(page: params[:page], per_page: 2)
   end
 
   def show
