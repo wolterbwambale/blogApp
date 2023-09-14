7713ea3f215b45ac7a380342ee4a98f1f5b131b2
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-09-14 17:34:22 +0300

    Add permits for account upadate  in the  update allowed para method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,8 @@ class ApplicationController < ActionController::Base
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :photo, :bio, :posts_counter, :email, :password) }
-
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :photo, :bio, :posts_counter, :email, :password, :current_password)
+    end
   end
 end
